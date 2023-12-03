504695fe62311725394624f32257c2394f1b1288
Devin <dbelle722@gmail.com>
2023-12-03 11:15:57 -0500

    Added Pseudo code

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,19 @@
 // Assignment Code
+
+//Will need to write out var charatcers you will be adding to each function below.
+//Symbols, Numbers, Uppercase, Lowercase, integerChoice:for number 8-128, option: character choices.
+
 var generateBtn = document.querySelector("#generate");
+
+//Need to define generatePassword() as a function
+function generatePassword(){
+  //define password length.
+  //step 1 prompt the user for the password criteria. Password length 8 < 128. Lowercase, uppercase, numbers, special characters.
+  //create a window prompt that asks for number of characters between 8-128
+  //create window prompt that asks for upper case, lowercase, special characters, and numbers,
+}
+
+//Define what var password is.
 
 // Write password to the #password input
 function writePassword() {
